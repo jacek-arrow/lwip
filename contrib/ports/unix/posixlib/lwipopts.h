09be1a0e70d4797b317f11734a49fbef1ebcb085
Jacek Naczyk <jacek.naczyk@arrow.com>
2023-10-12 14:19:54 +0200

    Switch to unix lib

--- contrib/ports/unix/posixlib/lwipopts.h
+++ contrib/ports/unix/posixlib/lwipopts.h
@@ -116,7 +116,7 @@
 #define LWIP_NOASSERT_ON_ERROR   1
 
 /* Threading options */
-/* #define LWIP_TCPIP_CORE_LOCKING   1 */
+#define LWIP_TCPIP_CORE_LOCKING   1
 
 /* If the system is 64 bit */
 #if defined __LP64__
